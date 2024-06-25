d2cd19d6e42f2b424dbc8ea834f374d66a431aef
v01p3f0x <v01p3f0x@gmail.com>
2024-06-25 21:41:41 +0000

    f

---
+++ .idx/dev.nix
@@ -0,0 +1,32 @@
+# To learn more about how to use Nix to configure your environment
+# see: https://developers.google.com/idx/guides/customize-idx-env
+{ pkgs, ... }: {
+  # Which nixpkgs channel to use.
+  channel = "stable-23.11"; # or "unstable"
+  # Use https://search.nixos.org/packages to find packages
+  packages = [
+    pkgs.nodejs_20
+    pkgs.python3
+    pkgs.hugo
+  ];
+  # Sets environment variables in the workspace
+  env = {};
+  idx = {
+    # Search for the extensions you want on https://open-vsx.org/ and use "publisher.id"
+    extensions = [
+      "GitHub.vscode-pull-request-github"
+      "golang.go"
+      "rusnasonov.vscode-hugo"
+    ];
+    # Enable previews and customize configuration
+    previews = {
+      enable = true;
+      previews = {
+        web = {
+          command = ["hugo" "server" "--config" "hugo.toml" "-p" "$PORT" "--bind" "0.0.0.0"];
+          manager = "web";
+        };
+      };
+    };
+  };
+}
